2cd9dd2100c8a420909be77999a513871a64e157
Yulong Tan <swifties@uw.edu>
2015-11-19 22:56:08 -0800

    created a package for weather

---
+++ app/src/main/java/yulongproductions/com/stormy/weather/Forecast.java
@@ -0,0 +1,10 @@
+package yulongproductions.com.stormy.weather;
+
+/**
+ * Created by Yulong on 11/19/2015.
+ */
+public class Forecast {
+    private Current mCurrent;
+    private Hour[] mHourlyForecast;
+    private Day[] mDailyForecast;
+}
