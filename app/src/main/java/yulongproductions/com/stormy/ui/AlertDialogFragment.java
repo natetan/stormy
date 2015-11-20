875352e08166507b8c399bfe08a66b8ecca982b4
Yulong Tan <swifties@uw.edu>
2015-11-19 22:55:50 -0800

    created a package for ui

---
+++ app/src/main/java/yulongproductions/com/stormy/ui/AlertDialogFragment.java
@@ -0,0 +1,25 @@
+package yulongproductions.com.stormy.ui;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+import yulongproductions.com.stormy.R;
+
+/**
+ * Created by Yulong on 8/11/2015.
+ */
+public class AlertDialogFragment extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.error_title))
+                .setMessage(context.getString(R.string.error_message))
+                .setPositiveButton(context.getString(R.string.error_ok_button_text), null);
+        return builder.create();
+    }
+}
