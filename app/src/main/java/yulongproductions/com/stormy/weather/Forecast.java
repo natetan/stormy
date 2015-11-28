ea4d11b6d174c5ee1618beadab4c5ffc4ff32541
Yulong Tan <swifties@uw.edu>
2015-11-27 23:43:14 -0800

    added iconId method into class

--- app/src/main/java/yulongproductions/com/stormy/weather/Forecast.java
+++ app/src/main/java/yulongproductions/com/stormy/weather/Forecast.java
@@ -1,5 +1,7 @@
 package yulongproductions.com.stormy.weather;
 
+import yulongproductions.com.stormy.R;
+
 /**
  * Created by Yulong on 11/19/2015.
  */
@@ -30,5 +32,31 @@ public class Forecast {
 
     public void setDailyForecast(Day[] dailyForecast) {
         mDailyForecast = dailyForecast;
+    }
+
+    public static int getIconId(String icon) {
+        int iconId = R.drawable.clear_day;
+        if (icon.equals("clear-day")) {
+            iconId = R.drawable.clear_day;
+        } else if (icon.equals("clear-night")) {
+            iconId = R.drawable.clear_night;
+        } else if (icon.equals("rain")) {
+            iconId = R.drawable.rain;
+        } else if (icon.equals("snow")) {
+            iconId = R.drawable.snow;
+        } else if (icon.equals("sleet")) {
+            iconId = R.drawable.sleet;
+        } else if (icon.equals("wind")) {
+            iconId = R.drawable.wind;
+        } else if (icon.equals("fog")) {
+            iconId = R.drawable.fog;
+        } else if (icon.equals("cloudy")) {
+            iconId = R.drawable.cloudy;
+        } else if (icon.equals("partly-cloudy-day")) {
+            iconId = R.drawable.partly_cloudy;
+        } else if (icon.equals("partly-cloudy-night")) {
+            iconId = R.drawable.cloudy_night;
+        }
+        return iconId;
     }
 }
