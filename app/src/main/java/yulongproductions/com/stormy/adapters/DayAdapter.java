e16f1d052349bdc5139a884f0145897e59b152c2
Yulong Tan <swifties@uw.edu>
2015-11-27 23:41:55 -0800

    implemented a recycler view for easy scrolling

--- app/src/main/java/yulongproductions/com/stormy/adapters/DayAdapter.java
+++ app/src/main/java/yulongproductions/com/stormy/adapters/DayAdapter.java
@@ -1,10 +1,14 @@
 package yulongproductions.com.stormy.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import yulongproductions.com.stormy.R;
 import yulongproductions.com.stormy.weather.Day;
 
 /**
@@ -22,14 +26,16 @@ public class DayAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return mDays.length;
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mDays[position];
     }
 
+    // Not going to use this method
+    // Used to tag items for easy reference
     @Override
     public long getItemId(int position) {
         return 0;
@@ -37,6 +43,31 @@ public class DayAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder;
+        // Brand new
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.daily_list_item, null);
+            holder = new ViewHolder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.iconImageView);
+            holder.temperatureLabel = (TextView) convertView.findViewById(R.id.temperatureLabel);
+            holder.dayLabel = (TextView) convertView.findViewById(R.id.dayNameLabel);
+
+            convertView.setTag(holder);
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        Day day = mDays[position];
+
+        holder.iconImageView.setImageResource(day.getIconId());
+        holder.temperatureLabel.setText(day.getTemperatureMax() + "");
+        holder.dayLabel.setText(day.getDayOfTheWeek());
         return null;
+    }
+
+    private static class ViewHolder {
+        ImageView iconImageView;
+        TextView temperatureLabel;
+        TextView dayLabel;
     }
 }
