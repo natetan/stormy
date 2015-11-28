391385d0f6ed4c8ce4e1ed1c6c6511d610f38229
Yulong Tan <swifties@uw.edu>
2015-11-27 23:07:04 -0800

    created a constructor for context and days

--- app/src/main/java/yulongproductions/com/stormy/adapters/DayAdapter.java
+++ app/src/main/java/yulongproductions/com/stormy/adapters/DayAdapter.java
@@ -1,13 +1,25 @@
 package yulongproductions.com.stormy.adapters;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+
+import yulongproductions.com.stormy.weather.Day;
 
 /**
  * Created by Yulong on 11/27/2015.
  */
 public class DayAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private Day[] mDays;
+
+    public DayAdapter(Context context, Day[] days) {
+        mContext = context;
+        mDays = days;
+    }
+
     @Override
     public int getCount() {
         return 0;
