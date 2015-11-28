55e27ddff31dffbfc731c2a443e2a4e5ba91ac9e
Yulong Tan <swifties@uw.edu>
2015-11-28 00:08:46 -0800

    used parcelable to get the array from MainActivity

--- app/src/main/java/yulongproductions/com/stormy/adapters/DayAdapter.java
+++ app/src/main/java/yulongproductions/com/stormy/adapters/DayAdapter.java
@@ -62,7 +62,7 @@ public class DayAdapter extends BaseAdapter {
         holder.iconImageView.setImageResource(day.getIconId());
         holder.temperatureLabel.setText(day.getTemperatureMax() + "");
         holder.dayLabel.setText(day.getDayOfTheWeek());
-        return null;
+        return convertView;
     }
 
     private static class ViewHolder {
