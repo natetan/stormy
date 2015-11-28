6bc4d37ba06bdae3e2f1316dfc1ab467e00922fd
Yulong Tan <swifties@uw.edu>
2015-11-28 00:09:42 -0800

    copied parcel array to new array

--- app/src/main/java/yulongproductions/com/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/yulongproductions/com/stormy/ui/DailyForecastActivity.java
@@ -1,21 +1,30 @@
 package yulongproductions.com.stormy.ui;
 
 import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Parcelable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 
+import java.util.Arrays;
+
 import yulongproductions.com.stormy.R;
+import yulongproductions.com.stormy.adapters.DayAdapter;
+import yulongproductions.com.stormy.weather.Day;
 
 public class DailyForecastActivity extends ListActivity {
+
+    private Day[] mDays;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_forecast);
 
+        /*
         String[] daysOfTheWeek = {
                 "Sunday",
                 "Monday",
@@ -29,6 +38,13 @@ public class DailyForecastActivity extends ListActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, daysOfTheWeek);
         setListAdapter(adapter);
+        */
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.DAILY_FORECAST);
+        mDays = Arrays.copyOf(parcelables, parcelables.length, Day[].class);
+
+        DayAdapter adapter = new DayAdapter(getBaseContext(), mDays);
     }
 
 
